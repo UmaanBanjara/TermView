6022dbcd28765dd87557fe385b784f996b063045
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-21 10:54:38 +0545

    added thumbnail picker

--- termview/lib/screens/createsession.dart
+++ termview/lib/screens/createsession.dart
@@ -1,75 +1,177 @@
-  import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:termview/helpers/imagepicker_web_helper.dart';
+import 'package:termview/helpers/validators.dart';
+import 'package:termview/widgets/snackbar.dart';
 
-  class Createsession extends StatefulWidget {
-    const Createsession({super.key});
+class Createsession extends StatefulWidget {
+  const Createsession({super.key});
 
-    @override
-    State<Createsession> createState() => _CreatesessionState();
-  }
+  @override
+  State<Createsession> createState() => _CreatesessionState();
+}
 
-  class _CreatesessionState extends State<Createsession> {
-    final TextEditingController _title = TextEditingController();
-    final TextEditingController _desc = TextEditingController();
-    @override
-    Widget build(BuildContext context) {
-      final text = Theme.of(context).textTheme;
-      return Scaffold(
-        body: Center(
+class _CreatesessionState extends State<Createsession> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _title = TextEditingController();
+  final TextEditingController _desc = TextEditingController();
+  bool _chatenabled = false;
+  Uint8List? _selectedImage;
+  
+  @override
+  Widget build(BuildContext context) {
+    final text = Theme.of(context).textTheme;
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Create New Session' , style: text.bodyLarge,),
-              SizedBox(height: 10,),
-              TextField(
+              Text('Create New Session', style: text.bodyLarge),
+              SizedBox(height: 10),
+              TextFormField(
                 controller: _title,
                 maxLines: 1,
                 cursorHeight: 22,
                 style: text.bodyMedium,
                 decoration: InputDecoration(
-                  hintText: "Enter the title of this session"
+                  hintText: "Enter the title for this session",
                 ),
+                validator: (value) => validateNotEmpty(value, "Title"),
               ),
-              SizedBox(height: 10,),
-              TextField(
+              SizedBox(height: 10),
+              TextFormField(
                 cursorHeight: 22,
                 maxLines: null,
                 controller: _desc,
                 style: text.bodyMedium,
                 decoration: InputDecoration(
-                  hintText: "Enter the description of this session"
+                  hintText: "Enter the description for this session",
+                ),
+                validator: (value) => validateNotEmpty(value, "Description"),
+              ),
+              SizedBox(height: 10),
+              CheckboxListTile(
+                value: _chatenabled,
+                title: Text("Enable Chat", style: text.bodyMedium),
+                onChanged: (bool? value) {
+                  setState(() {
+                    _chatenabled = value ?? false;
+                  });
+                },
+              ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: () async {
+                  final imageBytes =
+                      await ImagepickerWebHelper.pickImageFromGallery();
+                  if (imageBytes != null) {
+                    setState(() {
+                      _selectedImage = imageBytes;
+                    });
+                  } else {
+                    showTerminalSnackbar(
+                        context, "Failed to pick image",
+                        isError: true);
+                  }
+                },
+                child: Center(
+                  child: SizedBox(
+                    width: 200,
+                    child: AspectRatio(
+                      aspectRatio: 1 / 1,
+                      child: Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF2A2A2A),
+                              borderRadius: BorderRadius.circular(8),
+                              image: _selectedImage != null
+                                  ? DecorationImage(
+                                      image: MemoryImage(_selectedImage!),
+                                      fit: BoxFit.cover)
+                                  : null,
+                            ),
+                            child: _selectedImage == null
+                                ? Center(
+                                    child: Icon(
+                                      Icons.camera_alt,
+                                      size: 50,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                  )
+                                : null,
+                          ),
+                          if (_selectedImage != null)
+                            Positioned(
+                              top: 4,
+                              right: 4,
+                              child: GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _selectedImage = null;
+                                  });
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.black54,
+                                    shape: BoxShape.circle,
+                                  ),
+                                  padding: EdgeInsets.all(4),
+                                  child: Icon(
+                                    Icons.close,
+                                    size: 20,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 10,),
+              SizedBox(height: 20),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
-                    
-                    child: ElevatedButton(onPressed: (){
-
-                    }, 
-                    style: ElevatedButton.styleFrom(
-                      textStyle: text.bodyMedium,
-                      minimumSize: Size(0, 50)
-                    )
-                    ,child: Text('Start Session')),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print("Title: ${_title.text}");
+                          print("Description: ${_desc.text}");
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        textStyle: text.bodyMedium,
+                        minimumSize: Size(0, 50),
+                      ),
+                      child: Text('Start Session'),
+                    ),
                   ),
-                  SizedBox(width: 20,),
+                  SizedBox(width: 20),
                   Expanded(
-                    child: ElevatedButton(onPressed: (){
-                      Navigator.pop(context);
-                    }, 
-                    style: ElevatedButton.styleFrom(
-                      textStyle: text.bodyMedium,
-                      minimumSize: Size(0, 50),
-                    )
-                    ,child: Text('Cancel')),
-                  )
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        textStyle: text.bodyMedium,
+                        minimumSize: Size(0, 50),
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
