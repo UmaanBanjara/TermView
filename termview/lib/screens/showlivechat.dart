dc71c6042756cc6d5872a1a409f33d885de51854
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-22 10:34:36 +0545

    screen where user can view live chat if enabled

---
+++ termview/lib/screens/showlivechat.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:termview/helpers/chatsetting.dart';
+import 'package:termview/widgets/snackbar.dart';
+
+class Showlivechat extends StatefulWidget {
+  const Showlivechat({super.key});
+
+  @override
+  State<Showlivechat> createState() => _ShowlivechatState();
+}
+
+class _ShowlivechatState extends State<Showlivechat> {
+  final TextEditingController _chat = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    final text = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        leading: BackButton(),
+        centerTitle: false,
+        title: Text('Live Chat'  , style: text.bodyLarge,),
+        actions: [
+          ElevatedButton(onPressed: (){
+            chatsetting(context, onConfirm: (){
+              showTerminalSnackbar(context, 'Chat setting changed' , isError: false);
+            });
+          }, child: Text('Chat Setting')),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                width: double.infinity,
+                color: Colors.black87,
+                padding: EdgeInsets.all(8),
+                child: Center(
+                  child: Text('Chats' , style: text.bodyMedium,),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _chat,
+                    cursorHeight: 22,
+                    style: text.bodyMedium,
+                    decoration: InputDecoration(
+                      hintText: "What's on your mind",
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8,),
+                ElevatedButton(onPressed: (){
+        
+                }, 
+                style: ElevatedButton.styleFrom(
+                  textStyle: text.bodyMedium
+                )
+                ,child: Text('Send'))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
