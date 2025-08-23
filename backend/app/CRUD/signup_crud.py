7ac73978658b2411340c44653ed064fc7822a7a5
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-23 12:46:23 +0545

    added ORM function to create a new user

---
+++ backend/app/CRUD/signup_crud.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from app.models.users import User
+from app.testdb.db import SessionLocal
+
+#function to create a new user
+def create_user(username:str , email:str , password:str):
+    db: Session = SessionLocal()
+    try:
+        new_user = User(
+            username=username,
+            email=email,
+            password=password
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        db.close()
