7f9d9b6ca95447ac7aaf8c61d8b8b47559d517cb
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-24 14:07:20 +0545

    signup repo for api call

---
+++ termview/lib/data/repositories/signup_repository.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class SignupRepository {
+  final String baseUrl = "http://192.168.1.6:8000";
+
+  Future<Map<String , dynamic>> signup({
+    required String fname,
+    required String lname,
+    required String username,
+    required String email,
+    required String password,
+  })async{ 
+    final reponse = await http.post(
+      Uri.parse('$baseUrl/auth/signup'),
+      headers: {
+        "Content-Type" : 'application/json'
+      },
+      body: jsonEncode({
+        "fname" : fname,
+        "lname" : lname,
+        "username" : username,
+        "email" : email,
+        "password" : password
+      })
+    );
+    if(reponse.statusCode == 200){
+      return jsonDecode(reponse.body);
+    }else{
+      throw Exception(jsonDecode(reponse.body)['detail']??"Something went wrong");
+    }
+  }
+}
