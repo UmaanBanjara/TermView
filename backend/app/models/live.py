645b14209992d3992dc67db0e4f89e863298d2b7
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-28 13:58:45 +0545

    foreign key and one-many relationship added

--- backend/app/models/live.py
+++ backend/app/models/live.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column , Integer , String , Boolean
+from sqlalchemy import Column , Integer , String , Boolean, ForeignKey
 from app.testdb.db import Base
+from sqlalchemy.orm import relationship
 
 class Live(Base):
-    __tablename__ = "Live"
+    __tablename__ = "live"
 
     id = Column(Integer , primary_key = True , index = True)
     title = Column(String , nullable = False)
@@ -10,3 +11,9 @@ class Live(Base):
     thumb = Column(String)
     is_live = Column(Boolean , default = False , nullable=False)
 
+    #foreign key to user table
+    user_id = Column(Integer , ForeignKey("users.id"), nullable=False)
+
+    #optional : relationships for easy access
+
+    user = relationship("User" , back_populates="lives")
