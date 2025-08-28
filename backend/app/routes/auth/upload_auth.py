46f46e18a58265ff1a71def1ea13c7ea8c85157d
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-28 15:52:44 +0545

    extracts user_id from header to get current_user_id

--- backend/app/routes/auth/upload_auth.py
+++ backend/app/routes/auth/upload_auth.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter , UploadFile , File , Form , HTTPException
+from fastapi import APIRouter , UploadFile , File , Form , HTTPException, Header
 from app.CRUD.post_crud import create_post
 from app.utils.cloudinary import upload_to_cloudinary
+from app.utils.current_user import get_user_id_from_token
 
 router = APIRouter()
 
@@ -10,22 +11,28 @@ async def upload_thumbnail(
     title : str = Form(...),
     desc : str = Form(...),
     enable_chat : bool = Form(...),
+    authorization: str = Header(...)
+    
 
 ):  
+
+    #extract token from header
+    token = authorization.split(" ")[1]
+    user_id = get_user_id_from_token(token)
 
     #upload file to cloudinary via uplaod function
     upload = await upload_to_cloudinary(file)
     print("Upload response",upload)
 
     if "error" in upload:
-        return HTTPException(status_code = 500 , detail=upload["error"])
+        raise HTTPException(status_code = 500 , detail=upload["error"])
     
     url = upload.get("url") #get url
 
     if not url:
-        return HTTPException(status_code=500 , detail="No URL returned from Cloudinary")
+        raise HTTPException(status_code=500 , detail="No URL returned from Cloudinary")
     
-    new_post = create_post(title , desc , enable_chat , url , userId) #store in db
+    new_post = create_post(title , desc , enable_chat , url , user_id) #store in db
 
     return {
         "success" : True,
