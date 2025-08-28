40118b077aae67bb0051f0172ba4e6a7ce98b233
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-28 14:02:46 +0545

    repository for post creation , does all things related to api calling

---
+++ termview/lib/data/repositories/post_repository.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+
+
+class PostRepository {
+  final String baseUrl = 'http://192.168.1.6:8000';
+
+  Future<Map<String , dynamic>> post({
+    required String title,
+    required String desc,
+    required Uint8List fileBytes, //can't directly send file on web , only raw bytes
+    required String fileName,
+    required bool enableChat,
+  })async{
+    final url = Uri.parse('$baseUrl/auth/upload/thumbnail');
+    final request = http.MultipartRequest('POST', url);
+
+    request.files.add(await http.MultipartFile.fromBytes('file', fileBytes , filename: fileName)); // attach thumbnail & and send it as File Field
+
+    request.fields['title'] = title; // adding title form field
+    request.fields['desc'] = desc; // adding description form field
+    request.fields['enable_chat'] = enableChat.toString(); // adding enable_chat form field
+
+    final streamedResponse = await request.send();
+    final response = await http.Response.fromStream(streamedResponse);
+
+    if(response.statusCode == 200){
+      return jsonDecode(response.body);
+    }
+    else{
+      throw Exception("Failed to upload post : ${response.body}");
+    }
+    
+
+
+  }
+}
