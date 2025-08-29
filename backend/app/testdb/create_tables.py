67824180fbbab9ab7dea7b21d19b669aba8bb147
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-29 12:45:23 +0545

    helper to create tables

--- backend/app/testdb/create_tables.py
+++ backend/app/testdb/create_tables.py
@@ -1,6 +1,7 @@
 from app.testdb.db import engine, Base
 from app.models.users import User
 from app.models.live import Live
+from app.models.quiz import Quiz
 
 Base.metadata.create_all(bind=engine)
 print("Tables created successfully")
