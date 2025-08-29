f210b4454a2d3f8a9547f04a8e1112771c7f4238
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-29 12:52:14 +0545

    added pod that cods backend and changes the password

--- termview/lib/screens/forgotpass/resetpass.dart
+++ termview/lib/screens/forgotpass/resetpass.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:termview/data/notifiers/forpass_notifier.dart';
+import 'package:termview/data/providers/forpass_provider.dart';
+import 'package:termview/data/providers/login_provider.dart';
 import 'package:termview/helpers/validators.dart';
-import 'package:termview/screens/loginscreen.dart';
-import 'package:termview/widgets/page_transition.dart';
 import 'package:termview/widgets/snackbar.dart';
 
-class Resetpass extends StatefulWidget {
+class Resetpass extends ConsumerStatefulWidget {
   const Resetpass({super.key});
 
   @override
-  State<Resetpass> createState() => _ResetpassState();
+  ConsumerState<Resetpass> createState() => _ResetpassState();
 }
 
-class _ResetpassState extends State<Resetpass> {
+class _ResetpassState extends ConsumerState<Resetpass> {
   final _formkey = GlobalKey<FormState>();
+  final TextEditingController _old = TextEditingController();
   final TextEditingController _new = TextEditingController();
-  final TextEditingController _confirm = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final text = Theme.of(context).textTheme;
+    final forstate = ref.watch(ForpassnotifierProvider);
+    ref.listen<ForpassState>(ForpassnotifierProvider, (previous , next){
+      if(next.message != null && next.message != previous?.message){
+        showTerminalSnackbar(context, next.message! , isError: false);
+        Navigator.pop(context);
+      }
+      else if (next.error != null && next.error != previous?.error){
+        WidgetsBinding.instance.addPostFrameCallback((_){
+        showTerminalSnackbar(context, next.error! , isError: true);
+        return;
+        });
+        
+      }
+    });
     return Scaffold(
       body: Center(
         child: Form(
@@ -25,26 +42,26 @@ class _ResetpassState extends State<Resetpass> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Enter new and confirm your password' , style: text.bodyLarge,),
+              Text('Change Password' , style: text.bodyLarge,),
               SizedBox(height: 10,),
               TextFormField(
-                controller: _new,
+                controller: _old,
                 cursorHeight: 22,
                 obscureText: true,
                 style : text.bodyMedium,
                 decoration: InputDecoration(
-                  hintText: "Enter new password",
+                  hintText: "Enter old password",
                 ),
                 validator: (value) => validatePassword(value),
               ),
               SizedBox(height: 10,),
               TextFormField(
-                controller: _confirm,
+                controller: _new,
                 cursorHeight: 22,
                 style: text.bodyMedium,
                 obscureText: true,
                 decoration: InputDecoration(
-                  hintText: "Confirm your password",
+                  hintText: "Enter new password",
                 ),
                 validator: (value) => validatePassword(value),
               ),
@@ -53,9 +70,18 @@ class _ResetpassState extends State<Resetpass> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
-                    child: ElevatedButton(onPressed: (){
+                    child: 
+                    forstate.loading ? Center(child: SpinKitFadingFour(color: Colors.white,),)
+                    :
+                    ElevatedButton(onPressed: ()async{
                       if(_formkey.currentState!.validate()){
-                        showTerminalSnackbar(context, 'Update Clicked' , isError: false);
+                        final token = await ref.read(LoginnotifierProvider.notifier).getToken();
+                        if(token == null){
+                          showTerminalSnackbar(context, "User not logged in " , isError: true);
+                          return;
+                        }
+                        await ref.read(ForpassnotifierProvider.notifier).forpass(old_p: _old.text, new_p: _new.text, token: token);
+
                       }
                     }, 
                     style: ElevatedButton.styleFrom(
@@ -67,7 +93,7 @@ class _ResetpassState extends State<Resetpass> {
                   SizedBox(width: 20,),
                   Expanded(
                     child: ElevatedButton(onPressed: (){
-                      navigate(context, Loginscreen());
+                      Navigator.pop(context);
                     }, 
                     style: ElevatedButton.styleFrom(
                       textStyle: text.bodyMedium,
