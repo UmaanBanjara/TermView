3f14ccef517d02fd25fc49c6458ab4d38815e45e
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-29 12:46:16 +0545

    main file all sub routes gets connected here

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.auth import signup_auth
 from app.routes.auth import login_auth
 from app.routes.auth import upload_auth
+from app.routes.auth import changepass_auth
+from app.routes.auth import deleteacc_auth
+from app.routes.auth import quiz_auth
 
 app = FastAPI(title = "TermView API")
 
@@ -18,6 +21,9 @@ app.add_middleware(
 app.include_router(signup_auth.router , prefix="/auth" , tags=["Authentication"])
 app.include_router(login_auth.router , prefix="/auth" , tags=["Authorization"])
 app.include_router(upload_auth.router , prefix="/auth" , tags=["Upload"])
+app.include_router(changepass_auth.router , prefix="/auth" , tags=["Change Password"])
+app.include_router(deleteacc_auth.router , prefix="/auth" , tags=["Delete Account"])
+app.include_router(quiz_auth.router , prefix="/quiz" , tags=["Quiz"])
 
 #root endpoint
 @app.get("/")
