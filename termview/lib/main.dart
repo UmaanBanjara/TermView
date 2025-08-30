8190a52d1ff18e4374057e631c3aad4029041afb
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-30 11:19:53 +0545

    check auth persistant before starting the main app

--- termview/lib/main.dart
+++ termview/lib/main.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:termview/screens/homescreen.dart';
 import 'package:termview/screens/loginscreen.dart';
 import 'package:termview/theme/termview_theme.dart';
 
-void main(){
-  runApp(ProviderScope(child: MyApp()));
-}
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = FlutterSecureStorage();
+  final token = await storage.read(key: 'access_token');
 
-  @override
-  State<MyApp> createState() => _MyAppState();
+  runApp(
+    ProviderScope(
+      child: MyApp(
+        startScreen: token != null ? Homescreen() : Loginscreen(),
+      ),
+    ),
+  );
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  final Widget startScreen; 
+  const MyApp({required this.startScreen, super.key}); 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: TermviewTheme.darkTheme,
-      home: Loginscreen(),
+      home: startScreen, 
     );
   }
 }
