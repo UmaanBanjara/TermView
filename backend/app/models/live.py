1cee1776f088572f643f49f069a2c54f61b1c446
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-30 10:39:33 +0545

    added new column model for live

--- backend/app/models/live.py
+++ backend/app/models/live.py
@@ -1,19 +1,18 @@
-from sqlalchemy import Column , Integer , String , Boolean, ForeignKey
-from app.testdb.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
+from app.testdb.db import Base
 
 class Live(Base):
     __tablename__ = "live"
 
-    id = Column(Integer , primary_key = True , index = True)
-    title = Column(String , nullable = False)
-    desc = Column(String , nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    desc = Column(String, nullable=False)
     thumb = Column(String)
-    is_live = Column(Boolean , default = False , nullable=False)
+    is_live = Column(Boolean, default=False, nullable=False)
+    is_chat = Column(Boolean, default=False, nullable=False)
+    
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
-    #foreign key to user table
-    user_id = Column(Integer , ForeignKey("users.id"), nullable=False)
-
-    #optional : relationships for easy access
-
-    user = relationship("User" , back_populates="lives")
+    # optional: relationship to user
+    user = relationship("User", back_populates="lives")
