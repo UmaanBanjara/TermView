ddb712f6fe3737a37d1e64070678db7d0cae9eab
Umaan Banjara <umaanbanjara@gmail.com>
2025-08-30 12:32:10 +0545

    repository for fetchall endpoints calls the api

---
+++ termview/lib/data/repositories/fetchall_repository.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:termview/models/live_model.dart';
+
+class FetchallRepository {
+  final String baseUrl = 'http://192.168.1.6:8000';
+
+  Future<List<LiveModel>> fetchall()async{
+    final response = await http.get(Uri.parse('$baseUrl/fetch/fetchall'));
+
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      final session = data['sessions'] as List;
+      return session.map((json) => LiveModel.fromJson(json)).toList();
+
+    }
+    else{
+      throw Exception('Failed to Fetch Live Sessions');
+    }
+  }
+
+}
