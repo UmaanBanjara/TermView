237eadb4a882a41bb27613bf7341d731255af2c8
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-01 13:31:11 +0545

    page to view all quiz created in current session only UI no logic implemented yet

---
+++ termview/lib/screens/viewallquizes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Viewallquizes extends StatefulWidget {
+  const Viewallquizes({super.key});
+
+  @override
+  State<Viewallquizes> createState() => _ViewallquizesState();
+}
+
+class _ViewallquizesState extends State<Viewallquizes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
