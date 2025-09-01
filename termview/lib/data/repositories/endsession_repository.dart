6335d2cc7b1580114c20d93a6824bbfabb9c4b85
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-01 13:24:16 +0545

    sends seesion_id to knowwhich session to end

---
+++ termview/lib/data/repositories/endsession_repository.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class EndsessionRepository {
+  final String baseUrl = 'http://192.168.1.6:8000';
+
+  Future<Map<String,dynamic>> endsession({
+    required String post_id,
+  })async{
+    final response = await http.post(
+      Uri.parse('$baseUrl/end/endsession'),
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: jsonEncode(
+        {
+          "id" :int.parse(post_id)
+        }
+      )
+    );
+    if(response.statusCode==200){
+      return jsonDecode(response.body);
+    }
+    else{
+      throw Exception("Something went wrong. Please try again");
+    }
+  }
+}
