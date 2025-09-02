ae7f3885d9e3dd5c6d57e9781fbeb28a9734a7b2
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-02 12:18:08 +0545

    relationship with commands and chats added

--- backend/app/models/live.py
+++ backend/app/models/live.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from datetime import datetime
 from sqlalchemy.orm import relationship
 from app.testdb.db import Base
 import uuid
@@ -15,6 +16,17 @@ class Live(Base):
     
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     session_id = Column(String , unique=True, default=lambda: str(uuid.uuid4()))
+    created_at = Column(DateTime , default=datetime.utcnow, nullable=False)
 
     # optional: relationship to user
     user = relationship("User", back_populates="lives")
+
+    #relationship to commands
+
+    commands = relationship("Command" , back_populates="session" , cascade="all, delete-orphan")
+
+    #relationship to chats
+
+    chats= relationship("Chat" , back_populates="session" , cascade="all, delete-orphan")
+
+    
