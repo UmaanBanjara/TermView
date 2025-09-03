aa78a20eeea968b6c5e2d8498d78dfbe1bfdb2cc
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-03 12:59:25 +0545

    repository for live session , tries to start a new websocket channel / connection

--- termview/lib/data/repositories/live_session_repository.dart
+++ termview/lib/data/repositories/live_session_repository.dart
@@ -1,12 +1,20 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class LiveSessionRepository {
-  final String baseUrl = 'wss://termview.onrender.com/websocket/ws';
+  final String baseUrl = 'wss://termview.onrender.com/ws';
+  Future<WebSocketChannel> connect(String session_id)async{
+    try{
+      final channel = WebSocketChannel.connect(
+        Uri.parse('$baseUrl/$session_id')
+      );
 
+      await Future.delayed(Duration(milliseconds: 500));
 
-  WebSocketChannel connect(String sessionId){
-    return WebSocketChannel.connect(
-      Uri.parse('$baseUrl/$sessionId')
-    );
-  }
+      return channel;
+    }
+    catch(e){
+      throw Exception("Websocket connection failed : $e");
+    }
+  }//repo for livesesson
+  
 }
