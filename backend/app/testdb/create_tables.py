07e6a6c61a102ce346d6898e2463d6e110f579b0
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-03 11:05:27 +0545

    script to create tables

--- backend/app/testdb/create_tables.py
+++ backend/app/testdb/create_tables.py
@@ -2,6 +2,8 @@ from app.testdb.db import engine, Base
 from app.models.users import User
 from app.models.live import Live
 from app.models.quiz import Quiz
+from app.models.chat import Chat
+from app.models.commands import Command
 
 Base.metadata.create_all(bind=engine)
 print("Tables created successfully")
