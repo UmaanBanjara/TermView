678e9e2402b87a9421a85b296797f9465a1a1b64
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-04 13:32:18 +0545

    added print statement to check the logs

--- backend/app/websockets/session_ws.py
+++ backend/app/websockets/session_ws.py
@@ -33,5 +33,6 @@ async def websocket_endpoint(websocket : WebSocket , session_id : str):
         while True:
             data = await websocket.receive_text()
             await manager.broadcast(session_id , f"Host says : {data}")
+            print(f"Host Said : {data}")
     except WebSocketDisconnect:
         manager.disconnect(session_id , websocket)
