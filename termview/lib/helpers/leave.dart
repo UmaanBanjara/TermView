4d2702a5d76a761886b392645ad0a25cc908de29
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-05 12:26:33 +0545

    helper to disconnect from livesessoin using websocket.sink.close()

--- termview/lib/helpers/leave.dart
+++ termview/lib/helpers/leave.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
-Future<void> leavesession(BuildContext context , {required VoidCallback onConfirm}) async{
+
+import 'package:termview/widgets/snackbar.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+Future<void> leavesession(BuildContext context, WebSocketChannel? channel ,String sessionId) async{
+  final parentContext = context;
   final text = Theme.of(context).textTheme;
-  await showDialog(context: context, builder: (context){
+
+  if(channel == null){
+    showTerminalSnackbar(parentContext, "Not Connected" , isError: true);
+    return;
+  }
+  await showDialog(context: context, builder: (dialogContext){
     return AlertDialog(
       title:  Text('End Session' , style: text.bodyLarge,),
       content:  Text("Are you sure you want to leave this session?" , style: text.bodyMedium,),
       actions: [
         ElevatedButton(onPressed: (){
-          Navigator.pop(context);
+          Navigator.pop(dialogContext);
         }, child: const Text('No' )),
         ElevatedButton(onPressed: (){
-          Navigator.pop(context);
-          onConfirm();
+          Navigator.pop(dialogContext);
+
+          Future.microtask(()async{
+            try{
+              await channel.sink.close();
+              showTerminalSnackbar(parentContext, "Disconnected From Session" , isError: false);
+
+            }
+            catch(e){
+              showTerminalSnackbar(parentContext, "Connection error : $e" , isError: true);
+            }
+          });
         }, 
         style: ElevatedButton.styleFrom(
           textStyle: text.bodyMedium
