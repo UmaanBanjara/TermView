429754b921f53e126aeb7060454f3eadf86f1b8a
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-06 13:12:48 +0545

    now first checks token stored in device safe memory and send that token(jwt) to the api

--- termview/lib/data/repositories/live_session_repository.dart
+++ termview/lib/data/repositories/live_session_repository.dart
@@ -1,11 +1,15 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class LiveSessionRepository {
   final String baseUrl = 'wss://termview.onrender.com/ws';
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
   Future<WebSocketChannel> connect(String session_id)async{
     try{
+      final token = await _storage.read(key: "access_token");
+      if(token == null)throw Exception("Token Not Found");
       final channel = WebSocketChannel.connect(
-        Uri.parse('$baseUrl/$session_id')
+        Uri.parse('$baseUrl/$session_id?token=$token')
       );
 
       await Future.delayed(Duration(milliseconds: 500));
