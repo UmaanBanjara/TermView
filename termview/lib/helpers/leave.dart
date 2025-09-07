0ced46b5c81e4937409405dc7c7d9eeffd3e3a6c
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-07 09:54:36 +0545

    now does two things (sends endsesion as json data as well as query to db using notifier

--- termview/lib/helpers/leave.dart
+++ termview/lib/helpers/leave.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:termview/data/providers/endsession_provider.dart';
 import 'package:termview/screens/homescreen.dart';
 import 'package:termview/widgets/page_transition.dart';
 
 import 'package:termview/widgets/snackbar.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-Future<void> leavesession(BuildContext context, WebSocketChannel? channel ,String sessionId) async{
+Future<void> leavesession(BuildContext context, WebSocketChannel? channel ,String PostId  , WidgetRef ref) async{
   final parentContext = context;
   final text = Theme.of(context).textTheme;
 
@@ -18,7 +20,7 @@ Future<void> leavesession(BuildContext context, WebSocketChannel? channel ,Strin
   await showDialog(context: context, builder: (dialogContext){
     return AlertDialog(
       title:  Text('End Session' , style: text.bodyLarge,),
-      content:  Text("Are you sure you want to leave this session?" , style: text.bodyMedium,),
+      content:  Text("Are you sure you want to end this session?" , style: text.bodyMedium,),
       actions: [
         ElevatedButton(onPressed: (){
           Navigator.pop(dialogContext);
@@ -33,6 +35,9 @@ Future<void> leavesession(BuildContext context, WebSocketChannel? channel ,Strin
               }));
 
               await channel!.sink.close();
+              print("Before Notifier Call");
+              await ref.read(endsessionnotifierProvider.notifier).endsession(id: PostId);
+              print("After Notifier Call");
               showTerminalSnackbar(parentContext, "Session Ended Successfully" , isError: false);
               navigate(parentContext, Homescreen());
             }
