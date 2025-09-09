b81173768252fc8ff21dba29388635eb735b9774
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-09 11:05:38 +0545

    Fix loading spinner and token check on app start

--- termview/lib/main.dart
+++ termview/lib/main.dart
@@ -1,54 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:go_router/go_router.dart';
 import 'package:termview/screens/homescreen.dart';
-import 'package:termview/screens/joinedsession.dart';
 import 'package:termview/screens/loginscreen.dart';
 import 'package:termview/theme/termview_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final storage = FlutterSecureStorage();
-  final token = await storage.read(key: 'access_token');
-
   runApp(
     ProviderScope(
-      child: MyApp(
-        token: token,
-      ),
+      child: MyApp(),
     ),
   );
 }
-
 class MyApp extends StatelessWidget {
-  final String? token;
-  const MyApp({this.token, super.key}); 
+  const MyApp({super.key}); 
+
+  Future<String?> _getToken() async {
+    final _storage = FlutterSecureStorage();
+    return _storage.read(key: 'access_token');
+  }
 
   @override
   Widget build(BuildContext context) {
-    final router = GoRouter(
-      debugLogDiagnostics: false,
-      initialLocation: token != null ? '/' : '/login',
-      routes: [
-        GoRoute(path: '/', builder: (context, state) {
-          return Homescreen();
-        },),
-        GoRoute(path: '/login' , builder: (context , state){
-          return Loginscreen();
-        }),
-        GoRoute(path: '/live' , builder: (context , state){
-          final sessionId = state.uri.queryParameters['session_id'];
-          return Joinesesion(sessionId: sessionId,);
-        })
-      ]
-
-    );
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: TermviewTheme.darkTheme,
-      routerConfig: router,
+      home: FutureBuilder<String?>(
+        future: _getToken(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              backgroundColor: TermviewTheme.darkTheme.scaffoldBackgroundColor,
+              body: Center(
+                child: CircularProgressIndicator(color: Colors.blueAccent),
+              ),
+            );
+          }
 
+          final token = snapshot.data;
+          return token != null ? Homescreen() : Loginscreen();
+        },
+      ),
     );
   }
 }
