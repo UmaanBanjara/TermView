793ce0c761fba2c4072cd62d6049d4d607c1a009
Umaan Banjara <umaanbanjara@gmail.com>
2025-09-11 10:48:04 +0545

    Add function to save commands to the database

---
+++ backend/app/CRUD/command_crud.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from app.models.commands import Command
+from app.testdb.db import SessionLocal
+
+#function to save commands in db
+def new_command(session_id : str , command_txt : str):
+    db:Session = SessionLocal()
+    try:
+        command = Command(
+            session_id = session_id,
+            command_txt = command_txt
+        )
+        db.add(command)
+        db.commit(command)
+        db.refresh(command)
+        return command
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        db.close()
