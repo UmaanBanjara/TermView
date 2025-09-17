7e2d7c1db20c804d101bf710f661c981094acfae
Umaan Banjara <umaanbanjara.com>
2025-09-17 15:52:15 +0545

    Add endsession dialog to confirm and trigger session termination

--- termview/lib/helpers/end.dart
+++ termview/lib/helpers/end.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:termview/data/providers/endsession_provider.dart';
-Future<void> endsession(BuildContext context , WidgetRef ref , String sessionId) async{
+import 'package:termview/data/providers/sessionControllerProvider.dart';
+Future<void> endsession(BuildContext context,WidgetRef ref , String postId) async{
+  final parentContext = context;
   final text = Theme.of(context).textTheme;
-  await showDialog(context: context, builder: (context){
+
+
+  await showDialog(context: context, builder: (dialogContext){
     return AlertDialog(
       title:  Text('End Session' , style: text.bodyLarge,),
       content:  Text("Are you sure you want to end this session?" , style: text.bodyMedium,),
       actions: [
         ElevatedButton(onPressed: (){
-          Navigator.pop(context);
-        }, child: const Text('Cancel' )),
+          Navigator.pop(dialogContext);
+        }, child: const Text('No' )),
         ElevatedButton(onPressed: (){
-          Navigator.pop(context);
-          ref.read(endsessionnotifierProvider.notifier).endsession(id: sessionId);
+          Navigator.pop(dialogContext);
+          ref.read(endsessionnotifierProvider.notifier).endsession(id: postId);
+
+          ref.read(Sessioncontrollerprovider).endSession();
+          
           
         }, 
         style: ElevatedButton.styleFrom(
           textStyle: text.bodyMedium
         ),
-        child: Text('End'))
+        child: Text('Yes'))
       ],
     );
   });
