d75a804eac960e26ceb363f17f0e9679e8547010
Umaan Banjara <umaanbanjara.com>
2025-09-18 13:45:24 +0545

    Add SignupRepository to register new users via API

--- termview/lib/data/repositories/signup_repository.dart
+++ termview/lib/data/repositories/signup_repository.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class SignupRepository {
-  final String baseUrl = 'https://termview-backend.onrender.com';
+  final String baseUrl = 'your-render-url';
 
   Future<Map<String , dynamic>> signup({
     required String fname,
